93841ebff207296730828b4cf93c5c916b69f85d
yakerdev <yakerdev@gmail.com>
2025-01-27 19:37:54 +0100

    menu header updated

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const scrollers = document.querySelectorAll(".scroller");
 const services = document.querySelectorAll("#services-bar span")
+const menuButton = document.querySelector("#menu-button")
+const closeButton = document.querySelector("#close-button")
+const menuContainer = document.querySelector("#menu-container")
 
+
+
+
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.add("hidden")
+  closeButton.classList.remove("hidden")
+  menuContainer.style.display = "flex"
+})
+
+closeButton.addEventListener('click', () => {
+  menuButton.classList.remove("hidden")
+  closeButton.classList.add("hidden")
+  menuContainer.style.display = "none"
+})
 
 services.forEach((service) => {
   service.addEventListener("click", () => {
